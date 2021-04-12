771eaff056acd87b1c344a064d6b65d6e5587d3e
ldiebold <luke@ldiebold.com>
2021-04-12 21:54:43 +1000

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card>
+      <q-card-section>
+        My Card
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
